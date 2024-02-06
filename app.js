df984255d375e0fbf305811be681e80f839bc573
naveenguruu <naveenguru2113@gmail.com>
2024-02-06 18:14:24 +0530

    episode1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 const parent=React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"This is namaste"),
+    [React.createElement("h1",{},"This is namaste react"),
     React.createElement("h2",{},"I am h2 tag")]),
     React.createElement("div",{id:"child2"},
     [React.createElement("h1",{},"I am h1 tag"),
